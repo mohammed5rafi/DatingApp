1350c53ef1b6a925903c3da8f8fba738be3c0dba
Mohammed Rafi M. V <mohammedr@data-matica.com>
2021-05-23 01:06:18 +0530

    error -handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,56 @@
+using System;
+using Data;
+using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        public DataContext _context { get; set; }
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+
+        }
+        [Authorize]
+        [HttpGet("auth")]
+
+        public ActionResult<string> GetSecret(){
+            
+            return "Secret text";
+        }
+       
+        [HttpGet("not-found")]
+
+        public ActionResult<AppUser> GetNotFound(){
+            
+            var thing = _context.User.Find(-1);
+            if(thing == null) return NotFound();
+            return Ok(thing);
+        }
+ 
+        [HttpGet("server-error")]
+
+        public ActionResult<string> GetServerError(){
+            
+            
+                 var thing = _context.User.Find(-1);
+                var thingToReturn = thing.ToString();  
+         return thingToReturn;
+           
+     
+
+        }
+       
+        [HttpGet("bad-request")]
+
+        public ActionResult<string> GetBadRequest(){
+            
+            return BadRequest("This is not a good request");
+        }
+
+
+    }
+}
